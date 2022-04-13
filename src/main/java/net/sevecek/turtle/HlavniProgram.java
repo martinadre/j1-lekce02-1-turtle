73b0edaa31200e37260e9cb031b386a25edd981b
martina.dre <martina.dre@centrum.cz>
2022-04-13 20:25:40 +0200

    Želva nakreslila 4 domečky.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,17 +8,51 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
-        zofka.move(150);
-        zofka.turnRight(45);
-        zofka.move(90);
-        zofka.turnRight(90);
-        zofka.move(90);
-        zofka.turnRight(45);
-        zofka.move(150);
-        zofka.turnRight(90);
-        zofka.move(120);
 
-        
+        zofka.setLocation(485, 100);
+
+        zofka.turnRight(45);
+        zofka.move(90);
+        zofka.turnRight(90);
+        zofka.move(90);
+        zofka.turnRight(135);
+        zofka.move(125);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(125);
+        zofka.turnLeft(90);
+        zofka.move(150);
+
+        zofka.setLocation(50, 100);
+
+
+        for (int i = 0; i < 3; i++) {
+
+
+            zofka.turnRight(45);
+            zofka.move(90);
+            zofka.turnRight(90);
+            zofka.move(90);
+            zofka.turnRight(135);
+            zofka.move(125);
+            zofka.turnLeft(90);
+            zofka.move(150);
+            zofka.turnLeft(90);
+            zofka.move(125);
+            zofka.turnLeft(90);
+            zofka.move(150);
+
+            zofka.penUp();
+            zofka.turnRight(90);
+            zofka.move(20);
+            zofka.turnLeft(90);
+            zofka.penDown();
+        }
+
+
+
+
     }
 
 }
